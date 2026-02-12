c66e7229fbf7155647e3686a9274dc5f6d549cf7
Deric Jojo <deric.jojo@codecraft.co.in>
2026-02-12 06:22:59 +0000

    exported function for dynamic usage

--- arraySum.js
+++ arraySum.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var isTruthyArray_1 = require("./isTruthyArray");
 function arraySum(arr) {
     var sum = 0;
     for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
@@ -6,5 +9,10 @@ function arraySum(arr) {
     }
     return sum;
 }
-var array = [1, 2, 3, 4];
-console.log("sum of array [".concat(array, "] is ").concat(arraySum(array)));
+var array = [1, 2, 4, 5, 4];
+if ((0, isTruthyArray_1.isTruthyArray)(array)) {
+    console.log("sum of array [".concat(array, "] is ").concat(arraySum(array)));
+}
+else {
+    console.log("the array [".concat(array, "] has some falsy items"));
+}
--- arraySum.ts
+++ arraySum.ts
@@ -1,3 +1,5 @@
+import {isTruthyArray} from './isTruthyArray';
+
 function arraySum(arr:Array<number>):number {
     let sum = 0;
     for(const num of arr) {
@@ -7,5 +9,9 @@ function arraySum(arr:Array<number>):number {
     return sum;
 }
 
-let array:Array<number> = [1, 2, 3, 4];
-console.log(`sum of array [${array}] is ${arraySum(array)}`);
+let array:Array<any> = [1, 2, 4, 5, 4];
+if(isTruthyArray(array)) {
+    console.log(`sum of array [${array}] is ${arraySum(array)}`);
+} else {
+    console.log(`the array [${array}] has some falsy items`);
+}
